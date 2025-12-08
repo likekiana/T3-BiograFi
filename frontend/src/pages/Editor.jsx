10b8d69a1e8fd57ff58f89acbc2e3023d03a2b0e
朱子豪 <3147307592@qq.com>
2025-12-08 20:11:58 +0800

    修改按钮
    修改按钮

--- frontend/src/pages/Editor.jsx
+++ frontend/src/pages/Editor.jsx
@@ -1056,31 +1056,6 @@ return (
               className="author-input-compact"
             />
           </div>
-          <button
-            className="save-btn-compact"
-            onClick={handleManualSave}
-            disabled={saveStatus === 'saving'}
-            title={saveStatus === 'saving' ? '保存中...' : '保存文档'}
-          >
-            <i data-feather="save" data-rendered="false"></i>
-            {saveStatus === 'saving' ? '保存中' : t('save')}
-          </button>
-          
-          {/* 返回项目管理按钮 */}
-          <button
-            className="action-btn-compact"
-            onClick={() => {
-              if (onBack) {
-                onBack();
-              } else {
-                navigate('/projects');
-              }
-            }}
-            title="返回项目管理界面"
-          >
-            <i data-feather="arrow-left" data-rendered="false"></i>
-            返回项目
-          </button>
           
           {/* 删除文档按钮 */}
           <button
